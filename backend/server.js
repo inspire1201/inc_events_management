75ee7b70ef268f21e379f467a4a0ef58ca6a8bcc
inspire1201 <inspiretransoft2002@gmail.com>
2025-08-04 18:59:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,7 @@ const app = express();
 
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'http://inc-frontend.s3-website-us-east-1.amazonaws.com', // Example S3 URL
+  'http://localhost:5173'
 ];
 
 
@@ -45,7 +44,7 @@ app.use('/api', eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
