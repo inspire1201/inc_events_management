116231c6ea414635037592bb2a2424661651ca03
inspire1201 <inspiretransoft2002@gmail.com>
2025-08-08 19:45:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,3 +44,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
